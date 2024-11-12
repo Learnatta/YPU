e3709772bb9b7cee841b039d80b6edf0fb4a4a81
Learnatta <learnnattaypu@gmail.com>
2024-11-12 20:14:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const id = document.querySelector(".id");
+const pas = document.querySelector(".pas");
+const btn = document.querySelector(".btn");
+let init = false;
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (id.value == 12010111171 && pas.value == "97859zxccxzz") {
+    init = true;
+    window.location.href = "https://hamzaabduljalil.github.io/ma/";
+  }
+});

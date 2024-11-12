505e52a1b8692563bdc2031bff25a7080b119183
Learnatta <learnnattaypu@gmail.com>
2024-11-12 21:24:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".btn");
 let init = false;
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  if (id.value == 12010111171 && pas.value == "97859zxccxzz") {
+  if (id.value == 202110509 && pas.value == "12010111171") {
     init = true;
     window.location.href = "https://learnatta.github.io/markYPU/";
   }
